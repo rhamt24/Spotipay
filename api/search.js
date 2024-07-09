2c19d219dfb90feb5219114ffc05b4c7fc262b6c
Zals <rahmataprizal403@gmail.com>
2024-07-09 21:37:58 +0700

    search.js

--- api/search.js
+++ api/search.js
@@ -4,9 +4,8 @@ module.exports = async (req, res) => {
     try {
         const query = req.query.q;
         const results = await searching(query);
-        res.status(200).json(results);
+        res.status(200).json(results.data);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
 };
- 
