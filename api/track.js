e9692144dec8ab791c83d23c5b70e7038ce7a453
Zals <rahmataprizal403@gmail.com>
2024-07-09 21:31:02 +0700

    track.js

---
+++ api/track.js
@@ -0,0 +1,11 @@
+const { getInfo } = require('../lib/spotify');
+
+module.exports = async (req, res) => {
+    try {
+        const url = req.query.url;
+        const trackInfo = await getInfo(url);
+        res.status(200).json(trackInfo);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
