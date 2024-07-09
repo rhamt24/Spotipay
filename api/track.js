6e9556052a77d5dd0c9f323a7277c87ff547916d
Zals <rahmataprizal403@gmail.com>
2024-07-09 21:38:44 +0700

    track.js

--- api/track.js
+++ api/track.js
@@ -4,7 +4,7 @@ module.exports = async (req, res) => {
     try {
         const url = req.query.url;
         const trackInfo = await getInfo(url);
-        res.status(200).json(trackInfo);
+        res.status(200).json(trackInfo.data);
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
